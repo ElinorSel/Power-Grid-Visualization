df712b52b08a9b2c948198a0bc83454754212956
ElinorSel <124194603+ElinorSel@users.noreply.github.com>
2026-07-10 15:15:57 +0200

    create new edge and edge class

--- Assets/Scripts/Edge.cs
+++ Assets/Scripts/Edge.cs
@@ -4,37 +4,28 @@ using System.Collections.Generic;
 public class Edge
 {
     public string Id {get; set;}
-    private string type;
+
+    public bool InService {get;set;}
+    public float MaxLoad {get; set;}
+
     public Node Node1 {get; set;}
     public Node Node2 {get; set;}
-    public string VoltageLevel1Id {get; set;}
-    public string VoltageLevel2Id {get; set;}
-    private bool isConnected1;
-    private bool isConnected2;
-    public float Power {get; set;}
-    public float ReactivePower {get; set;}
-    private float current;
-    public float NormalMVALimit {get; set;}
 
-    public float Load {get; set;}
-
-    public Edge(string id, string type, string voltageLevel1id, bool isConnected1, string voltageLevel2id, bool isConnected2, float power, float reactivePower, float current, float normalMVALimit)
+    public Edge(string id, bool inService, float maxload, Node node1, Node node2)
     {
         this.Id = id;
-        this.type = type;
-        this.VoltageLevel1Id = voltageLevel1id;
-        this.isConnected1 = isConnected1;
-        this.VoltageLevel2Id = voltageLevel2id;
-        this.isConnected2 = isConnected2;
-        this.Power = power;
-        this.ReactivePower = reactivePower;
-        this.current = current;
-        this.NormalMVALimit = normalMVALimit;
+        this.InService = inService;
+        this.MaxLoad = maxload;
+        this.Node1 = node1;
+        this.Node2 = node2;
     }
 
+/*
     public void DebugPrintData()
     {
         Debug.Log("Edge ID: " + Id + ", Edge Type: " + type + ", Voltage Level 1 ID: " + VoltageLevel1Id + "Is Connected 1: " + isConnected1 + ", Voltage Level 2 ID: " + VoltageLevel2Id + ", Is Connected 2: " + isConnected2 + ", Power: " + Power + ", Reactive Power: " + ReactivePower + ", Current: " + current + ", Normal MVA Limit: " + NormalMVALimit);
 
     }
+
+    */
 }
