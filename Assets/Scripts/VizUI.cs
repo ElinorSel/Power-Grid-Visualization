d6987a5f6f13e718aaf0f780fbf11f0b35affd6c
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-07-29 10:05:10 +0200

    Fixed UI for time range

--- Assets/Scripts/VizUI.cs
+++ Assets/Scripts/VizUI.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class VizUI : MonoBehaviour
 {
+    [SerializeField] TMP_Text minLabel;
+    [SerializeField] TMP_Text maxLabel;
    public void ToggleLabels()
     {
         Debug.Log("VIZ UI: trying to sett labels to " + !VisualizationSettings.Instance.ShowLabels);
@@ -40,6 +43,22 @@ public class VizUI : MonoBehaviour
     public void SetSizeScaleFactor(float value)
     {
         VisualizationSettings.Instance.SetSizeScaleFactor(value);
+    }
+
+    public void SetTimeRange(float minValue, float maxValue)
+    {
+        Debug.Log("On values changed min: " + minValue + "max: " + maxValue);
+        
+    }
+
+    public void UpdateTimeRangeMinLabel(float value)
+    {
+        minLabel.text = value.ToString();
+    }
+
+    public void UpdateTimeRangeMaxLabel(float value)
+    {
+        maxLabel.text = value.ToString();
     }
 
 
